8cb93389419680961beb2c55233a59d5a3888bd8
220110720 <220110720@comp2.hwccluster.labcenter>
2024-12-09 08:20:45 +0000

    demo done

--- fs/demo/src/main.c
+++ fs/demo/src/main.c
@@ -44,9 +44,30 @@ static void* demo_mount(struct fuse_conn_info * conn_info){
 
 
     /* 填充super信息 */
-    super.sz_io = /* TODO */;
-    super.sz_disk = /* TODO */;
-    super.sz_blks = /* TODO */; 
+    /*
+        int ddriver_ioctl(int fd, unsigned long cmd, void *ret);
+        cmd: IOC_REQ_DEVICE_SIZE 请求设备大小
+             IOC_REQ_DEVICE_IO_SZ 请求设备 I/O 大小
+        ret: 用于接收来自设备驱动程序的数据
+    */
+    if (ddriver_ioctl(super.driver_fd, IOC_REQ_DEVICE_IO_SZ, &super.sz_io) == -1) {
+        perror("Failed to get device I/O size");
+        ddriver_close(super.driver_fd);
+        return (void*)-1;
+    }
+    printf("Device I/O size: %d bytes\n", super.sz_io);
+
+    // 获取磁盘总大小
+    if (ddriver_ioctl(super.driver_fd, IOC_REQ_DEVICE_SIZE, &super.sz_disk) == -1) {
+        perror("Failed to get device size");
+        ddriver_close(super.driver_fd);
+        return (void*)-1;
+    }
+    printf("Device size: %d bytes\n", super.sz_disk);
+
+    // 设置逻辑块大小为I/O大小的两倍
+    super.sz_blks = 2 * super.sz_io;
+    printf("Block size set to: %d bytes\n", super.sz_blks);
 
     return 0;
 }
@@ -67,12 +88,28 @@ static int demo_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off
     /* 根据超级块的信息，从第500逻辑块读取一个dentry，ls将只固定显示这个文件名 */
 
     /* TODO: 计算磁盘偏移off，并根据磁盘偏移off调用ddriver_seek移动磁盘头到磁盘偏移off处 */
+    off_t off = super.sz_blks * 500;                 // 第500个逻辑块的偏移
+    /*
+        int ddriver_seek(int fd, off_t offset, int whence);
+        fd: 文件描述符
+        offset: 偏移量
+        whence: 参考点，在 fs.h 中定义，SEEK_SET(0)是相对于文件开头
+    */
+    ddriver_seek(super.driver_fd, off, SEEK_SET);    // 将磁盘头移动
 
     /* TODO: 调用ddriver_read读出一个磁盘块到内存，512B */
+    char readBuf[super.sz_io];                              // 缓冲区大小一个逻辑块
+    /*
+        int ddriver_read(int fd, char *buf, size_t size);
+    */
+    ddriver_read(super.driver_fd, readBuf, super.sz_io);    // 读取存入缓冲区
 
     /* TODO: 使用memcpy拷贝上述512B的前sizeof(demo_dentry)字节构建一个demo_dentry结构 */
+    struct demo_dentry dentry;
+    memcpy(&dentry, readBuf, sizeof(dentry));
 
     /* TODO: 填充filename */
+    strcpy(filename, dentry.fname);
 
     // 此处大家先不关注filler，已经帮同学写好，同学填充好filename即可
     return filler(buf, filename, NULL, 0);
@@ -84,7 +121,7 @@ static int demo_getattr(const char* path, struct stat *stbuf)
     if(strcmp(path, "/") == 0)
         stbuf->st_mode = DEMO_DEFAULT_PERM | S_IFDIR;            // 根目录是目录文件
     else
-        stbuf->st_mode = /* TODO: 显示为普通文件 */;            // 该文件显示为普通文件
+        stbuf->st_mode = 0644 | S_IFREG;            // 该文件显示为普通文件
     return 0;
 }
 
